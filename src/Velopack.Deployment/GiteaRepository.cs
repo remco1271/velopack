34e0c217f14010208a6865836464950084ca9496
Remco Fischer <6353023+remco1271@users.noreply.github.com>
2024-06-05 20:55:56 +0200

    Use async functions
    Added code to get all release for name check
    All API calls are async
    Use WithHttpInfo to get status code instead of a throw

--- src/Velopack.Deployment/GiteaRepository.cs
+++ src/Velopack.Deployment/GiteaRepository.cs
@@ -1,17 +1,13 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Text;
+using Gitea.Net.Api;
+using Gitea.Net.Client;
+using Gitea.Net.Model;
 using Microsoft.Extensions.Logging;
 using Velopack.NuGet;
 using Velopack.Packaging;
 using Velopack.Packaging.Exceptions;
 using Velopack.Sources;
-using Gitea.Net.Client;
-using Amazon.S3.Model;
-using Gitea.Net.Model;
-using Gitea.Net.Api;
-using System.Diagnostics;
-using System.Net.Mail;
-using Attachment = Gitea.Net.Model.Attachment;
-using System.IO;
 
 namespace Velopack.Deployment;
 
@@ -86,47 +82,44 @@ public class GiteaRepository : SourceRepository<GiteaDownloadOptions, GiteaSourc
         Log.Info($"Preparing to upload {build.Files.Count} asset(s) to Gitea");
 
         // Set token if provided
-        if(!string.IsNullOrWhiteSpace(options.Token)) {
+        if (!string.IsNullOrWhiteSpace(options.Token)) {
             config.ApiKey.Add("token", options.Token);
         }
         var apiInstance = new RepositoryApi(config);
-        //var existingReleases = await client.Repository.Release.GetAll(repoOwner, repoName);
-        Release existingReleases = null;
-        if (!options.Merge) {
-
-            try {
-                existingReleases = apiInstance.RepoGetReleaseByTag(repoOwner, repoName, semVer.ToString());
-                if (existingReleases != null) 
-                {
-                    throw new UserInfoException($"There is already an existing release tagged '{semVer}'. Please delete this release or provide a new version number.");
-                }
-            } 
-            catch (Gitea.Net.Client.ApiException ex) 
-            {
-                // API throws when release is not found
-                if (ex.ErrorCode == 404)
-                    Log.Debug("Release not found with tag");
-                else
-                    throw;
-            } 
-            catch (Exception) 
-            {
-                throw;
+        // Get all releases
+        // Get repository info for total releases
+        List<Release> existingReleases = null;
+        ApiResponse<Repository> repositoryInfo = await apiInstance.RepoGetWithHttpInfoAsync(repoOwner, repoName);
+        if (repositoryInfo != null && repositoryInfo.StatusCode == HttpStatusCode.OK) {
+            // Get all releases
+            var allReleases = await apiInstance.RepoListReleasesWithHttpInfoAsync(repoOwner, repoName, page: 1, limit: (int) repositoryInfo.Data.ReleaseCounter);
+            existingReleases = allReleases.Data;
+            if (allReleases != null && allReleases.StatusCode == HttpStatusCode.OK && allReleases.Data.Any(r => r.Name == releaseName)) {
+                throw new UserInfoException($"There is already an existing release named '{releaseName}'. Please delete this release or provide a new release name.");
             }
-            //if (existingReleases.Any(r => r.Name == releaseName)) {
-            //    throw new UserInfoException($"There is already an existing release named '{releaseName}'. Please delete this release or provide a new release name.");
-            //}
+        } else {
+            throw new UserInfoException("Could not get all releases from server");
+        }
+        
+        if (!options.Merge) {
+            ApiResponse<Release> httpInfo = await apiInstance.RepoGetReleaseByTagWithHttpInfoAsync(repoOwner, repoName, semVer.ToString());
+            // If a release with tag is found it will return 200, if not found it returns 404
+            if (httpInfo != null && httpInfo.StatusCode == HttpStatusCode.OK) {
+                throw new UserInfoException($"There is already an existing release tagged '{semVer}'. Please delete this release or provide a new version number.");
+            }
+            if (existingReleases.Any(r => r.Name == releaseName)) {
+                throw new UserInfoException($"There is already an existing release named '{releaseName}'. Please delete this release or provide a new release name.");
+            }
         }
 
-        // create or retrieve Gitea release
-        Release release = new Release();
-        //var release = existingReleases.FirstOrDefault(r => r.TagName == semVer.ToString())
-        //    ?? existingReleases.FirstOrDefault(r => r.Name == releaseName);
+        // create or retrieve github release
+        var release = existingReleases.FirstOrDefault(r => r.TagName == semVer.ToString())
+            ?? existingReleases.FirstOrDefault(r => r.Name == releaseName); ;
 
-        if (existingReleases != null) {
-            if (existingReleases.TagName != semVer.ToString())
-                throw new UserInfoException($"Found existing release matched by name ({existingReleases.Name} [{existingReleases.TagName}]), but tag name does not match ({semVer}).");
-            Log.Info($"Found existing release ({existingReleases.Name} [{existingReleases.TagName}]). Merge flag is enabled.");
+        if (release != null) {
+            if (release.TagName != semVer.ToString())
+                throw new UserInfoException($"Found existing release matched by name ({release.Name} [{release.TagName}]), but tag name does not match ({semVer}).");
+            Log.Info($"Found existing release ({release.Name} [{release.TagName}]). Merge flag is enabled.");
         } else {
             var newReleaseReq = new CreateReleaseOption(semVer.ToString()) {
                 Body = releaseNotes,
@@ -136,7 +129,7 @@ public class GiteaRepository : SourceRepository<GiteaDownloadOptions, GiteaSourc
                 TargetCommitish = options.TargetCommitish,
             };
             Log.Info($"Creating draft release titled '{newReleaseReq.Name}'");
-            release = apiInstance.RepoCreateRelease(repoOwner, repoName, newReleaseReq);
+            release = await apiInstance.RepoCreateReleaseAsync(repoOwner, repoName, newReleaseReq);
         }
 
         // check if there is an existing releasesFile to merge
@@ -156,17 +149,15 @@ public class GiteaRepository : SourceRepository<GiteaDownloadOptions, GiteaSourc
         };
         var json = ReleaseEntryHelper.GetAssetFeedJson(feed);
 
-        await RetryAsync(() => {
-            Attachment response = apiInstance.RepoCreateReleaseAttachment(repoOwner, repoName, release.Id, releasesFileName, new MemoryStream(Encoding.UTF8.GetBytes(json)));
-            return Task.FromResult(response);
+        await RetryAsync(async () => {
+            await apiInstance.RepoCreateReleaseAttachmentAsync(repoOwner, repoName, release.Id, releasesFileName, new MemoryStream(Encoding.UTF8.GetBytes(json)));
         }, "Uploading " + releasesFileName);
 
         if (options.Channel == ReleaseEntryHelper.GetDefaultChannel(RuntimeOs.Windows)) {
             var legacyReleasesContent = ReleaseEntryHelper.GetLegacyMigrationReleaseFeedString(feed);
             var legacyReleasesBytes = Encoding.UTF8.GetBytes(legacyReleasesContent);
-            await RetryAsync(() => {
-                Attachment response = apiInstance.RepoCreateReleaseAttachment(repoOwner, repoName, release.Id, "RELEASES", new MemoryStream(legacyReleasesBytes));
-                return Task.FromResult(response);
+            await RetryAsync(async () => {
+                await apiInstance.RepoCreateReleaseAttachmentAsync(repoOwner, repoName, release.Id, "RELEASES", new MemoryStream(legacyReleasesBytes));
             }, "Uploading legacy RELEASES (compatibility)");
         }
 
@@ -188,11 +179,10 @@ public class GiteaRepository : SourceRepository<GiteaDownloadOptions, GiteaSourc
         }
     }
 
-    private Task UploadFileAsAsset(RepositoryApi client, Release release, string repoOwner, string repoName, string filePath)
+    private async Task UploadFileAsAsset(RepositoryApi client, Release release, string repoOwner, string repoName, string filePath)
     {
         using var stream = File.OpenRead(filePath);
         // Create a release attachment
-        Attachment response = client.RepoCreateReleaseAttachment(repoOwner, repoName, release.Id, Path.GetFileName(filePath), stream);
-        return Task.FromResult(response);
+        await client.RepoCreateReleaseAttachmentAsync(repoOwner, repoName, release.Id, Path.GetFileName(filePath), stream);
     }
 }
