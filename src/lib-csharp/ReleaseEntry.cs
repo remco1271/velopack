784f918299138b9f7f285c70a8d1a5120e105349
Remco Fischer <6353023+remco1271@users.noreply.github.com>
2024-10-15 23:14:22 +0200

    Update ReleaseEntry.cs
    Allow for spaces in file names

--- src/lib-csharp/ReleaseEntry.cs
+++ src/lib-csharp/ReleaseEntry.cs
@@ -183,7 +183,7 @@ namespace Velopack
         //    return zp.IconUrl;
         //}
 
-        static readonly Regex entryRegex = new Regex(@"^([0-9a-fA-F]{40})\s+(\S+)\s+(\d+)[\r]*$");
+        static readonly Regex entryRegex = new Regex(@"^([0-9a-fA-F]{40})\s+(\S[\S ]*?)\s+(\d+)[\r]*$");
         static readonly Regex commentRegex = new Regex(@"\s*#.*$");
         static readonly Regex stagingRegex = new Regex(@"#\s+(\d{1,3})%$");
 
